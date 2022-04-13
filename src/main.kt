957335b2df4479d1fd360a4a73076c46a6aa9f47
saido-noor <saidoomarnoor@gmail.com>
2022-04-13 09:41:19 +0300

    operators

--- src/main.kt
+++ src/main.kt
@@ -1,39 +1,113 @@
+import org.w3c.dom.css.CSS2Properties
+
 fun main(){
-    takeName("Ada")
-
-    var result = modulus(8,3)
-    println(result)
-
-   var total= sum(456, 432,1,1)
+  myFriend("Saido")
+  var total= add(1,4,56, 58)
     println(total)
-    printFact("Meditation")
-
 
 }
 
 
-
-fun takeName(name:String){
-    println("Hello $name")
+fun myFriend(name:String){
+    println("myFriend $name")
 }
-
-fun modulus(numb1:Int, numb2:Int):Int{
-    var modulus = numb1%numb2
-    return modulus
-
-}
-
-fun sum(num1: Int, num2: Int, num3:Int, num4:Int):Int{
-    var sum = num1 + num2 + num3 + num4
+fun add(numb3:Int,numb5:Int, numb7:Int, numb8:Int):Int{
+    var sum = numb5 + numb3 + numb7 + numb8
     return sum
 }
-
-fun printFact(fact:String){
-    println("I love $fact")
-}
-
-fun printFact(fact:String){
-    println("I love $fact")
-}
+//fun multiply(numb3: Int, numb12:Int, numb4:Int ){
+//    var product = numb3*
+//}
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//fun main(){
+//    takeName("Ada")
+//
+//    var result = modulus(8,3)
+//    println(result)
+//
+//   var total= sum(456, 432,1,1)
+//    println(total)
+//    printFact("Meditation")
+//
+//    multiply(7,2)
+//    subtraction(10,5)
+//
+//
+//
+//}
+//
+//
+//
+//fun takeName(name:String){
+//    println("Hello $name")
+//}
+//
+//fun modulus(numb1:Int, numb2:Int):Int{
+//    var modulus = numb1%numb2
+//    return modulus
+//
+//}
+//
+//fun sum(num1: Int, num2: Int, num3:Int, num4:Int):Int{
+//    var sum = num1 + num2 + num3 + num4
+//    return sum
+//}
+//
+//fun printFact(fact:String){
+//    println("I love $fact")
+//}
+//
+//fun multiply(numbers1: Int, numbers2: Int){
+//    var product = numbers1 * numbers2
+//    println(product)
+//}
+//
+//fun subtraction(nu1:Int, nu2:Int){
+//    var difference = nu1 - nu2
+//    println(difference)
+//}
